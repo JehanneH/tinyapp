865e813caaf5727f84ee8ab89aeed6400ba691b4
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-08 19:55:06 -0400

    implement generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,13 @@
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+};
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -38,3 +44,10 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
