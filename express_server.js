0a76efa0abbae8f78bb04f1a43d30b92ea7c666b
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-09 21:38:00 -0400

    add login/logout with cookies

--- express_server.js
+++ express_server.js
@@ -6,9 +6,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,16 +30,24 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { 
+    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -49,6 +59,11 @@ app.get("/u/:shortURL", (req, res) => {
     res.status(404);
     res.end();
   }
+});
+app.post('/urls', (req, res) => {
+  const tiny = generateRandomString();
+  urlDatabase[tiny] = req.body.longURL;
+  res.redirect(`/urls/ ${tiny}`);
 });
 
 app.post("/urls/:shortURL", (req, res) => {
@@ -63,13 +78,12 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
-
-
-app.post('/urls', (req, res) => {
-  const tiny = generateRandomString();
-  urlDatabase[tiny] = req.body.longURL;
-  res.redirect(`/urls/ ${tiny}`);
+app.post('/login', (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect('/urls')
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
